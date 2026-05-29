d744fd722d871c6600c899216cf4c24c5744344d
Hema Sri Sravya Padala <sravyapadala59@gmail.com>
2026-05-29 21:54:28 +0530

    Added conditional statement programs

---
+++ day3.py
@@ -0,0 +1,19 @@
+# Even or Odd Program
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# Greater Number Program
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print(a, "is greater")
+else:
+    print(b, "is greater")
