c80ef2762896cfeba44fd5804fd1048369f3fbc2
Hema Sri Sravya Padala <sravyapadala59@gmail.com>
2026-05-30 21:49:31 +0530

    Create day4.py
    Added loop practice programs

---
+++ day4.py
@@ -0,0 +1,13 @@
+# Day 4 - Loops
+
+# Program 1: Print numbers from 1 to 10
+
+for i in range(1, 11):
+    print(i)
+
+# Program 2: Multiplication Table
+
+num = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)
