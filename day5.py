6a53d21923f27bd7e2ad167ef93d94bd765f940e
Hema Sri Sravya Padala <sravyapadala59@gmail.com>
2026-05-31 21:03:05 +0530

    Create day5.py
    Added basic function programs

---
+++ day5.py
@@ -0,0 +1,16 @@
+# Day 5 - Functions
+
+# Program 1: Greeting Function
+
+def greet():
+    print("Hello, Welcome!")
+
+greet()
+
+
+# Program 2: Display Name Function
+
+def show_name():
+    print("Padala Hema Sri Sravya")
+
+show_name()
