c24d11fc05da567142fa160fcfb79888ce988e14
Hema Sri Sravya Padala <sravyapadala59@gmail.com>
2026-06-01 23:38:14 +0530

    Create day6.py
    Added list practice programs

---
+++ day6.py
@@ -0,0 +1,11 @@
+# Day 6 - Lists
+
+# Program 1: Create a List
+
+fruits = ["Apple", "Mango", "Orange"]
+print(fruits)
+
+# Program 2: Access List Elements
+
+print("First Fruit:", fruits[0])
+print("Second Fruit:", fruits[1])
