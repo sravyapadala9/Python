8f7c2233983b6d914ee217eb1c2b1c97c7de10db
Hema Sri Sravya Padala <sravyapadala59@gmail.com>
2026-06-04 22:57:09 +0530

    Create day9.py
    Added dictionary practice program

---
+++ day9.py
@@ -0,0 +1,12 @@
+# Day 9 - Dictionaries
+
+student = {
+    "name": "Sravya",
+    "age": 19,
+    "course": "B.Tech"
+}
+
+print("Student Details")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
