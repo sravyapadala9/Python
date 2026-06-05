0d0647186d39157dd63df6ffa413380cc960f295
Hema Sri Sravya Padala <sravyapadala59@gmail.com>
2026-06-05 22:02:49 +0530

    Create day 1 0b.py
    Added list operations practice

---
+++ day 1 0b.py
@@ -0,0 +1,10 @@
+# Day 10B - Lists
+
+fruits = ["Apple", "Mango", "Orange"]
+
+print("Fruit List:", fruits)
+
+fruits.append("Banana")
+
+print("Updated List:", fruits)
+print("Total Fruits:", len(fruits))
